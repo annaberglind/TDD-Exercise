247fc474f84ec4f2929ec0def517ab682bc985cb
annaberglind <annaj11@ru.is>
2013-11-04 16:39:31 +0000

    held ég sé núna að adda öllum testunum sem ég er búin að gera (smá misskilningur), var buin að gera fleiri commit, gleymdi að adda

--- src/Main/Java/is/ru/stringcalculator/Calculator.java
+++ src/Main/Java/is/ru/stringcalculator/Calculator.java
@@ -1,5 +1,22 @@
-package is.ru.stringcalculator
+package is.ru.stringcalculator;
 
 public class Calculator {
+
+	public static int add(String text){
+
+		if (text.equals("1")){
+			return 1;
+		}
+		else if(text.contains(",")){
+			String[] numbers = text.split(",");
+			return toInt(numbers[0]) + toInt(numbers[1]);
+		}
+		else
+			return 0;
+	}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
 	
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,8 +1,25 @@
-package is.ru.stringcalculator
+package is.ru.stringcalculator;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
 	
+	@Test
+	public void Emptystring(){
+		assertEquals(0,Calculator.add(""));
+	}
+
+	@Test
+	public void onestring() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void twostrings() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
+
+
 }
